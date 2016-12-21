36e45d1ddf1b4f87c52cef8ff2288ec92427247d
k8hertweck <k8hertweck@gmail.com>
2016-12-21 10:34:22 -0600

    adding data summary, correcting figures

--- HNSCC.R
+++ HNSCC.R
@@ -3,30 +3,96 @@
 install.packages("dplyr")
 install.packages("tidyr")
 install.packages("ggplot2")
+install.packages("stringr")
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(string)
 
 # import data
-system("sed 's/(.*$//g' data/HNSCCgeneData.csv > temp")
-dat <- read.delim("temp")
-system("rm temp")
+# complete data
+dat <- read.delim("data/Ot6699_Ot6700_Santanu.Dasgupta_300-923_hEx-AV4_30x_01132013_Cancer_Nucleotide_Variation.csv")
+# all somatic data
+datSom <- read.delim("data/all_somatic.csv")
+# target gene somatic data
+datTarget <- read.delim("data/targetGenes.csv")
+# target gene unique somatic data
+datUnique <- read.delim("data/target_unique.csv")
 
-# plot by gene and type of mutation
-ggplot(dat, aes(gene, fill=where_in_gene)) + geom_bar(position="dodge") + geom_bar(position="dodge", colour="black", show.legend=FALSE) + scale_fill_brewer(palette="Set1", name="location in\ngene") + ylab("number of mutations") + theme_bw()
-ggsave(file="figures/mutations.pdf")
+# assess number of mutations across whole genome
+str(datSom) #25045 somatic mutations in whole genome
+# tally genome-wide somatic mutations by type (SNP, MNP, etc)
+datSom %>%
+  group_by(kind_cancer) %>%
+  tally()
+# tally CDS: 1868 somatic mutations in CDS
+datSom %>%
+  filter(where_in_gene == "CDS") %>%
+  tally()
+# tally CDS by gene: 1868 somatic mutations in CDS
+allGeneTally <- datSom %>%
+  filter(where_in_gene == "CDS") %>%
+  group_by(gene) %>%
+  tally()
+allGeneTally %>%
+  filter(n >5)
+# evaluate syn, nonsyn, and frameshift (37) in all CDS
+datSom %>%
+  filter(where_in_gene == "CDS") %>%
+  filter(str_detect(annot_cancer, "synonymous")) %>%
+  tally() # 726 synonymous; 5 also listed as nonsyn so count as 721
+datSom %>%
+  filter(where_in_gene == "CDS") %>%
+  filter(str_detect(annot_cancer, "frameshift")) %>%
+  tally() # 37 frameshift
+datSom %>%
+  filter(where_in_gene == "CDS") %>%
+  filter(str_detect(annot_cancer, "->")) %>%
+  tally() # 1110 nonsynonymous
+# evaluate zygosity
 
-# plot by synonymous/nonsynonymous
-CDS <- dat %>%
+
+# assess number of somatic mutations per target gene
+tarAll <- datTarget %>%
+  group_by(gene) %>%
+  tally()
+tarCDS <- datTarget %>%
+  filter(where_in_gene == "CDS") %>%
+  group_by(gene) %>%
+  tally()
+tarNonsym <- datTarget %>%
+  filter(where_in_gene == "CDS") %>%
+  filter(str_detect(annot_cancer, "synonymous")) %>%
+  group_by(gene) %>%
+  tally()
+tarUnique <- datTarget %>%
+  filter(where_in_gene == "CDS") %>%
+  filter(str_detect(annot_cancer, "synonymous")) %>%
+  filter(str_detect(dbsnp, "rs")) %>%
+  group_by(gene) %>%
+  tally()
+CDS.Nonsyn.Uniq.Target <- cbind(tarCDS, tarNonsym$n, tarUnique$n)
+
+# plot number of somatic mutations in target genes (total) 
+ggplot(datTarget, aes(gene)) + geom_bar(colour="black", show.legend=FALSE) + ylab("number of mutations") + theme_bw()
+ggsave(file="figures/2C.totalSomaticMutationsTargetGenes.pdf")
+# plot number of somatic mutations in target genes (total) 
+ggplot(filter(datTarget, where_in_gene == "CDS"), aes(gene)) + geom_bar(colour="black", show.legend=FALSE) + ylab("number of mutations") + theme_bw()
+ggsave(file="figures/2C.CDS.SomaticMutationsTargetGenes.pdf")
+# plot number of somatic mutations in target genes (novel) 
+ggplot(datUnique, aes(gene)) + geom_bar(colour="black", show.legend=FALSE) + ylab("number of mutations") + theme_bw()
+ggsave(file="figures/2D.novelSomaticMutationsTargetGenes.pdf")
+
+# plot number of somatic mutations in target genes based on location in gene
+ggplot(datTarget, aes(gene, fill=where_in_gene)) + geom_bar(position="dodge", colour="black", show.legend=FALSE) + scale_fill_brewer(palette="Set1", name="location in\ngene") + ylab("number of mutations") + theme_bw()
+ggsave(file="figures/2A.TargetGeneSomaticMutationLocation.pdf")
+
+# plot target gene somatic mutations by synonymous/nonsynonymous
+CDS <- datTarget %>%
   select(gene, where_in_gene, annot_cancer) %>%
   filter(where_in_gene == "CDS")
+CDS$annot_cancer <- sub("synonymous (.*)", "synonymous", CDS$annot_cancer)
 CDS$annot_cancer <- gsub(".*->.*", "nonsynonymous", CDS$annot_cancer)
   
-ggplot(CDS, aes(gene, fill=annot_cancer)) + geom_bar(position="dodge") + geom_bar(position="dodge", colour="black", show.legend=FALSE) + scale_fill_brewer(palette="Set1", name="") + ylab("number of mutations") + theme_bw()
-ggsave(file="figures/synnonsyn.pdf")
-
-# import filtered data
-filt <- read.delim("data/HNSCCgeneTable.csv") 
-
-ggplot(filt, aes(gene)) +geom_bar() + ylab("number of mutations") + theme_bw()
-ggsave(file="figures/filterednonsyn.pdf")
+ggplot(CDS, aes(gene, fill=annot_cancer)) + geom_bar(position="dodge", colour="black", show.legend=FALSE) + scale_fill_brewer(palette="Set1", name="") + ylab("number of mutations") + theme_bw()
+ggsave(file="figures/2B.TargetGeneSomaticSynNonSyn.pdf")
