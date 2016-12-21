1ce369e721ac197a51c19bda777ca5c89eb7b404
k8hertweck <k8hertweck@gmail.com>
2016-12-20 21:02:21 -0600

    correcting strict somatic calls

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Genomic analysis of HPV-induced oropharyngeal cancer (paired tumor/normal sample
 Workflow:
 * sequencing, SNP calling and annotation performed by Otogenetics (http://www.otogenetics.com)
 * raw sequence data can be found at NCBI SRA #SRP081299
-* `data/` contains: (to be released with publication)
-	* somatic SNP data (`1Ot6699_Ot6700_Santanu.Dasgupta_300-923_hEx-AV4_30x_01132013_Cancer_Nucleotide_Variation`)
-	* SNP data from target genes (`HNSCCgeneData.csv`)
-	* summarized, unique SNPs (`HNSCCgeneTable.csv`)
+* `data/` contains:
+	* `Ot6699_Ot6700_Santanu.Dasgupta_300-923_hEx-AV4_30x_01132013_Cancer_Nucleotide_Variation`: somatic variation, includes somatic variants and loss of heterozygotes (
+	* `variantTypes.txt`: all possible variants (comparing normal and cancer)
+	* `all_somatic.csv`: original file strictly filtered for only somatic variants (LOH removed)
+	* `HNSCC_*.csv`: all somatic variants, filtered by CDS, nonsynonymous, then unique (not in dbSNP)
+	* `target_*.csv`: somatic variants from target genes, filtered by CDS, nonsynonymous, then unique (not in dbSNP)
 * `candidateGenes.lst` is a list of target genes
 * `table.sh` is the script used to derive the `.csv` files in `data/`
 * `HNSCC.R` is the R script used to obtain figures from parsed gene data
--- table.sh
+++ table.sh
@@ -3,37 +3,43 @@
 ## Data format
 ##	$DATA with the following columns: 1 chr, 2 left_cancer (position), 15 gene, 17 annot_cancer
 
-DATA=~/Dropbox/cancerGenomics/HNSCC/data/Ot6699_Ot6700_Santanu.Dasgupta_300-923_hEx-AV4_30x_01132013_Cancer_Nucleotide_Variation.csv
 SCRIPTS=~/GitHub/HNSCCtargetGenes
 
-cd ~/Dropbox/cancerGenomics/HNSCC/analysis
+cd $SCRIPTS/data
+
+## all data
+# extract all possible variants
+cut -f 10,11 Ot6699*.csv | sort | uniq -c > variantTypes.txt
+
+# extract all strictly somatic variants
+awk '{if($10 == "a/a" || $10 == "c/c" || $10 == "t/t" || $10 == "g/g" || $10 == "/") next;
+	else print $0}' Ot6699*.csv > all_somatic.csv
+
+# extract CDS from somatic
+head -1 all_somatic.csv > HNSCC_CDS.csv
+grep "\tCDS\t" all_somatic.csv >> HNSCC_CDS.csv
+# extract nonsynonymous from CDS
+grep -v synonymous HNSCC_CDS.csv > HNSCC_nonsyn.csv
+# extract unique from nonsynonymous 
+grep -v rs[0-9]* HNSCC_nonsyn.csv > HNSCC_unique.csv
 
 ## target genes
 # extract all data from target genes
+head -1 all_somatic.csv > targetGenes.csv
 for x in `cat $SCRIPTS/candidateGenes.lst`
 	do
-		grep $x $DATA >> targetGenes.csv
+		grep $x Ot6699*.csv >> targetGenes.csv
 done	
-
-# extract CDS, nonsynonymous, unique	
-head -1 $DATA > target_CDS.csv
-grep CDS targetGenes.csv >> target_CDS.csv
+# extract CDS from somatic	
+head -1 all_somatic.csv > target_CDS.csv
+grep "\tCDS\t" targetGenes.csv >> target_CDS.csv
 # extract nonsynonymous from CDS
 grep -v synonymous target_CDS.csv > target_nonsyn.csv
 # extract unique from nonsynonymous (inspect zygosity to ensure somatic)
 grep -v rs[0-9]* target_nonsyn.csv > target_unique.csv
 # format for publication table
 echo -e "gene\tchromosome\tposition\tmutation\ttype" > temp
-cut -f 1,2,8,15,17 target_unique.csv | sed 's/(.*$//g' | awk -F "\t" 'BEGIN{OFS="\t"}{print $4,$1,$2,$5,$3}' >> temp
+tail +2 target_unique.csv | cut -f 1,2,8,15,17 | sed 's/(.*$//g' | awk -F "\t" 'BEGIN{OFS="\t"}{print $4,$1,$2,$5,$3}' >> temp
 # clean up mutation types
 sed 's/SNP/missense/' temp | sed 's/MNP/missense/' | sed 's/INS/frameshift/' | sed 's/DEL/frameshift/' > HNSCCgeneTable.csv
 rm temp
-
-## all data
-# extract data from coding sequences
-head -1 $DATA > HNSCC_CDS.csv
-grep CDS $DATA >> HNSCC_CDS.csv
-# extract nonsynonymous from CDS
-grep -v synonymous HNSCC_CDS.csv > HNSCC_nonsyn.csv
-# extract unique from nonsynonymous 
-grep -v rs[0-9]* HNSCC_nonsyn.csv > HNSCC_unique.csv
